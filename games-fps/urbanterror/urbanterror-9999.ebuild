holgersson32644/holgersson-overlay

commit f45de05215cc3c5227585ffbf1e1085daa342194

[games-fps/urbanterror] Added USE="voip" for internal sound chat support.

games-fps/urbanterror/urbanterror-9999.ebuild
@@ -16,7 +16,7 @@ EGIT_BRANCH="urt"
 LICENSE="GPL-2"
 SLOT="0"
 KEYWORDS=""
-IUSE="+altgamma +curl debug dedicated mumble openal +opus server +sdl vorbis"
+IUSE="+altgamma +curl debug dedicated mumble openal +opus server +sdl voip vorbis"
 PATCHES=(
 	"${FILESDIR}"/${P}-respect_CFLAGS.patch
 )
@@ -65,20 +65,20 @@ src_compile() {
 		BUILD_STANDALONE=1 \
 		SERVERBIN="Quake3-UrT-Ded" \
 		CLIENTBIN="Quake3-UrT" \
-		USE_RENDERER_DLOPEN=0 \
+		USE_RENDERER_DLOPEN=1 \
 		USE_YACC=0 \
 		BASEGAME="q3ut4"\
 		BASEGAME_CFLAGS="${CFLAGS}" \
 		USE_SDL=$(buildit sdl) \
 		USE_SDL_DLOPEN=$(buildit sdl) \
 		USE_OPENAL=$(buildit openal) \
-		USE_OPENAL_DLOPEN=0 \
+		USE_OPENAL_DLOPEN=1 \
 		USE_CURL=$(buildit curl) \
-		USE_CURL_DLOPEN=0 \
+		USE_CURL_DLOPEN=1 \
 		USE_CODEC_VORBIS=$(buildit vorbis) \
 		USE_CODEC_OPUS=$(buildit opus) \
 		USE_MUMBLE=$(buildit mumble) \
-		USE_VOIP=0 \
+		USE_VOIP=$(buildit voip) \
 		USE_INTERNAL_LIBS=0 \
 		USE_INTERNAL_ZLIB=1 \
 		USE_LOCAL_HEADERS=0 \

games-fps/urbanterror/metadata.xml
@@ -17,7 +17,8 @@
 		<flag name="altgamma">Alternative way for gamma setting with modesetting driver</flag>
 		<flag name="dedicated">Build only server target - dedicated to hosting a server</flag>
 		<flag name="opus">Use opus as sound codec</flag>
-		<flag name="mumble">Use Mumble for in-game VoIP (currently not supported by upstream)</flag>
+		<flag name="mumble">Use Mumble for in-game VoIP (experimental)</flag>
+		<flag name="voip">Use ioq3 ingame VoIP (experimental)</flag>
 		<flag name="server">Build server target</flag>
 	</use>
 	<upstream>

commit dadec3a01e86a17054e90680c87623f297c42981

[games-fps/urbanterror] Some fixes.
	Fixes a regression I introduced in commit f45de05215cc3c5227585ffbf1e1085daa342194:
	Fixes some other choices to make sense (_DL_OPEN if USE is actually set)
	modified:   urbanterror-9999.ebuild

games-fps/urbanterror/urbanterror-9999.ebuild
@@ -63,16 +63,16 @@ src_compile() {
 		BUILD_STANDALONE=1 \
 		SERVERBIN="Quake3-UrT-Ded" \
 		CLIENTBIN="Quake3-UrT" \
-		USE_RENDERER_DLOPEN=1 \
+		USE_RENDERER_DLOPEN=0 \
 		USE_YACC=0 \
 		BASEGAME="q3ut4"\
 		BASEGAME_CFLAGS="${CFLAGS}" \
 		USE_SDL=$(buildit sdl) \
 		USE_SDL_DLOPEN=$(buildit sdl) \
 		USE_OPENAL=$(buildit openal) \
-		USE_OPENAL_DLOPEN=1 \
+		USE_OPENAL_DLOPEN=$(buildit openal) \
 		USE_CURL=$(buildit curl) \
-		USE_CURL_DLOPEN=1 \
+		USE_CURL_DLOPEN=$(buildit curl) \
 		USE_CODEC_VORBIS=$(buildit vorbis) \
 		USE_CODEC_OPUS=$(buildit opus) \
 		USE_MUMBLE=$(buildit mumble) \